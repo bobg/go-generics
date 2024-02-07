1a3ff500664588f16f67b46e22019ce2bc6fcb7e
Bob Glickstein <bobg@emphatic.com>
2024-02-06 19:46:54 -0800

    Update for Go 1.22, and add types and functions in anticipation of Go 1.23 iterators.

--- iter/iter.go
+++ iter/iter.go
@@ -24,3 +24,66 @@ type Of[T any] interface {
 	// It may be called only after Next returns false.
 	Err() error
 }
+
+// Seq is a Go 1.23 iterator over sequences of individual values.
+// When called as seq(yield), seq calls yield(v) for each value v in the sequence,
+// stopping early if yield returns false.
+//
+// This type is defined in the same way as in the standard library,
+// but is not identical,
+// because Go type aliases cannot (yet?) be used with generic types.
+type Seq[V any] func(yield func(V) bool)
+
+// Seq2 is a Go 1.23 iterator over sequences of pairs of values, most commonly key-value pairs.
+// When called as seq(yield), seq calls yield(k, v) for each pair (k, v) in the sequence,
+// stopping early if yield returns false.
+//
+// This type is defined in the same way as in the standard library,
+// but is not identical,
+// because Go type aliases cannot (yet?) be used with generic types.
+type Seq2[K, V any] func(yield func(K, V) bool)
+
+// All makes a Go 1.23 iterator from an Of[T],
+// suitable for use in a one-variable for-range loop.
+//
+// The caller should still check the iterator's Err method after the loop terminates.
+func All[T any](inp Of[T]) Seq[T] {
+	return func(yield func(T) bool) {
+		for inp.Next() {
+			if !yield(inp.Val()) {
+				return
+			}
+		}
+	}
+}
+
+// AllCount makes a counting iterator from an Of[T],
+// suitable for use in a two-variable for-range loop.
+//
+// The caller should still check the iterator's Err method after the loop terminates.
+func AllCount[T any](inp Of[T]) Seq2[int, T] {
+	return func(yield func(int, T) bool) {
+		var i int
+		for inp.Next() {
+			if !yield(i, inp.Val()) {
+				return
+			}
+			i++
+		}
+	}
+}
+
+// AllPairs makes a pair iterator from an Of[Pair[T, U]],
+// suitable for use in a two-variable for-range loop.
+//
+// The caller should still check the iterator's Err method after the loop terminates.
+func AllPairs[T, U any](inp Of[Pair[T, U]]) Seq2[T, U] {
+	return func(yield func(T, U) bool) {
+		for inp.Next() {
+			p := inp.Val()
+			if !yield(p.X, p.Y) {
+				return
+			}
+		}
+	}
+}
---
+++ iter/iter_test.go
@@ -0,0 +1,65 @@
+package iter
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestAll(t *testing.T) {
+	var (
+		ints   = Ints(1, 1)      // All integers starting at 1
+		first5 = FirstN(ints, 5) // First 5 integers
+	)
+
+	testSeq(t, All(first5), []int{1, 2, 3, 4, 5})
+}
+
+func TestAllCount(t *testing.T) {
+	names := FromSlice([]string{"Alice", "Bob", "Carol"})
+
+	testSeq2(t, AllCount(names), []int{0, 1, 2}, []string{"Alice", "Bob", "Carol"})
+}
+
+func TestAllPairs(t *testing.T) {
+	var (
+		letters = FromSlice([]string{"a", "b", "c", "d"})
+		nums    = FromSlice([]int{1, 2, 3})
+		pairs   = Zip(letters, nums)
+	)
+
+	testSeq2(t, AllPairs(pairs), []string{"a", "b", "c", "d"}, []int{1, 2, 3, 0})
+}
+
+func testSeq[T any](t *testing.T, seq Seq[T], want []T) {
+	var got []T
+
+	seq(func(val T) bool {
+		got = append(got, val)
+		return true
+	})
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func testSeq2[T, U any](t *testing.T, seq Seq2[T, U], wantT []T, wantU []U) {
+	var (
+		gotT []T
+		gotU []U
+	)
+
+	seq(func(valT T, valU U) bool {
+		gotT = append(gotT, valT)
+		gotU = append(gotU, valU)
+		return true
+	})
+
+	if !reflect.DeepEqual(gotT, wantT) {
+		t.Errorf("got %v, want %v", gotT, wantT)
+	}
+
+	if !reflect.DeepEqual(gotU, wantU) {
+		t.Errorf("got %v, want %v", gotU, wantU)
+	}
+}
